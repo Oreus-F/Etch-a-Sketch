acf04e84e1f67a61f37dd37d0192698f6afd117a
Oreus-F <fourniol.lucas@gmail.com>
2024-10-03 19:31:53 +0200

    Create grid with one function
    assuming the grid will always be the same amount of columns and row, we can put only one parameters wich will define the grid in every for loop

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const container = document.querySelector(".container");
+
+function makeGrid(gridNumb){
+    for (let i = 0; i < gridNumb; i++){
+        const column = document.createElement("div");
+        container.appendChild(column).setAttribute("class", "gridColumn");
+    };
+    const allColumns = document.querySelectorAll(".gridColumn");
+
+    allColumns.forEach((column) => {
+        for (let j= 0; j< gridNumb; j++) {
+            const cell = document.createElement("div");
+            column.appendChild(cell).setAttribute("class", "gridRow");
+        };
+    });
+    const allCells = document.querySelectorAll(".gridRow");
+    allCells.forEach((cell) => {
+        cell.addEventListener("mouseover", function(e) {
+            e.target.style.background = "lightblue";
+        });
+    });
+
+};
+
+
+makeGrid(80);
